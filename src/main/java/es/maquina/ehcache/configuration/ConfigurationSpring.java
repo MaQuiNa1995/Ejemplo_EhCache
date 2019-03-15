1b27a3a8ba616f780099f5c1876a64192f7452a7
MaQuiNa1995 <maquina1995@gmail.com>
2019-03-15 19:40:01 +0100

    Añadida la configuracion

---
+++ src/main/java/es/maquina/ehcache/configuration/ConfigurationSpring.java
@@ -0,0 +1,75 @@
+package es.maquina.ehcache.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+public class ConfigurationSpring {
+	private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+	private static final String PROPERTY_NAME_HIBERNATE_MAX_FETCH_DEPTH = "hibernate.max_fetch_depth";
+	private static final String PROPERTY_NAME_HIBERNATE_JDBC_FETCH_SIZE = "hibernate.jdbc.fetch_size";
+	private static final String ENTITYMANAGER_PACKAGES_TO_SCAN = "es.cic.curso.curso18.mascotastarcraft.dominio";
+
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+		dataSource.setUrl("jdbc:hsqldb:mem:maquina1995");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		dataSource.setInitialSize(5);
+		dataSource.setMaxActive(10);
+		dataSource.setPoolPreparedStatements(Boolean.TRUE);
+		dataSource.setMaxOpenPreparedStatements(10);
+		return dataSource;
+
+	}
+
+	@Bean
+	public JpaTransactionManager jpaTransactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+		return transactionManager;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setJpaVendorAdapter(vendorAdaptor());
+		entityManagerFactoryBean.setDataSource(dataSource());
+		entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		entityManagerFactoryBean.setPackagesToScan(ENTITYMANAGER_PACKAGES_TO_SCAN);
+		entityManagerFactoryBean.setJpaProperties(jpaHibernateProperties());
+
+		return entityManagerFactoryBean;
+	}
+
+	private HibernateJpaVendorAdapter vendorAdaptor() {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setShowSql(true);
+		vendorAdapter.setDatabase(Database.HSQL);
+		vendorAdapter.setGenerateDdl(Boolean.TRUE);
+
+		return vendorAdapter;
+	}
+
+	private Properties jpaHibernateProperties() {
+
+		Properties properties = new Properties();
+
+		properties.put(PROPERTY_NAME_HIBERNATE_MAX_FETCH_DEPTH, 10);
+		properties.put(PROPERTY_NAME_HIBERNATE_JDBC_FETCH_SIZE, 2);
+		properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, "org.hibernate.dialect.MySQLDialect");
+		return properties;
+	}
+
+}
---
+++ src/main/java/es/maquina/ehcache/configuration/LiquibaseConfig.java
@@ -0,0 +1,38 @@
+package es.maquina.ehcache.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+@Configuration
+public class LiquibaseConfig {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public SpringLiquibase liquibase() {
+
+		String changelogFile = "classpath:/maquina/ehchache/changelog.xml";
+
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog(changelogFile);
+		liquibase.setContexts("test");
+		liquibase.setDataSource(dataSource);
+		liquibase.setDropFirst(true);
+		liquibase.setShouldRun(true);
+
+		Map<String, String> params = new HashMap<>();
+		params.put("verbose", "true");
+		liquibase.setChangeLogParameters(params);
+
+		return liquibase;
+	}
+}
